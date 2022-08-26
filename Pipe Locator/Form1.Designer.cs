22c47600a1e74e9109485def459e64a7a1c7eb90
Lux14 <99058499+Magnito14@users.noreply.github.com>
2022-08-25 23:14:31 -0400

    Add files via upload

---
+++ Pipe Locator/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Pipe_Locator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.copyToClipboardToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearListboxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(5, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Find All Pipes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Log Newly Created";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(200, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(56, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Stop";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(155, 172);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Total Pipes: ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 169);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 20);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = "Search Pipes";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.copyToClipboardToolStripMenuItem,
+            this.clearListboxToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(172, 48);
+            // 
+            // copyToClipboardToolStripMenuItem
+            // 
+            this.copyToClipboardToolStripMenuItem.Name = "copyToClipboardToolStripMenuItem";
+            this.copyToClipboardToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.copyToClipboardToolStripMenuItem.Text = "Copy to Clipboard";
+            this.copyToClipboardToolStripMenuItem.Click += new System.EventHandler(this.copyToClipboardToolStripMenuItem_Click);
+            // 
+            // clearListboxToolStripMenuItem
+            // 
+            this.clearListboxToolStripMenuItem.Name = "clearListboxToolStripMenuItem";
+            this.clearListboxToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.clearListboxToolStripMenuItem.Text = "Clear Pipes";
+            this.clearListboxToolStripMenuItem.Click += new System.EventHandler(this.clearListboxToolStripMenuItem_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(5, 5);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(251, 160);
+            this.listBox1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(261, 218);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Named Pipe Locator v 1.0";
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem copyToClipboardToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearListboxToolStripMenuItem;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Pipe Locator/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Pipe_Locator
+{
+    public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void OnChanged(object source, FileSystemEventArgs e)
+		{
+			base.Invoke(new MethodInvoker(delegate ()
+			{
+				string item = e.FullPath.Replace("\\\\.\\pipe\\", string.Empty);
+				this.listBox1.Items.Add(item);
+				this.listBox1.Refresh();
+				this.label1.Text = "Total Pipes: " + this.listBox1.Items.Count;
+			}));
+		}
+
+		private void OnDeleted(object source, FileSystemEventArgs e)
+		{
+			base.Invoke(new MethodInvoker(delegate ()
+			{
+				string value = e.FullPath.Replace("\\\\.\\pipe\\", string.Empty);
+				this.listBox1.Items.Remove(value);
+				this.listBox1.Refresh();
+				this.label1.Text = "Total Pipes: " + this.listBox1.Items.Count;
+			}));
+		}
+
+		private FileSystemWatcher watcher = new FileSystemWatcher();
+
+		private string[] listOfPipes = Directory.GetFiles("\\\\.\\pipe\\");
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			this.listBox1.Items.Clear();
+			this.listBox1.Refresh();
+			string[] array = this.listOfPipes;
+			for (int i = 0; i < array.Length; i++)
+			{
+				string item = array[i].Replace("\\\\.\\pipe\\", string.Empty);
+				this.listBox1.Items.Add(item);
+			}
+			this.watcher.Path = "\\\\.\\pipe\\";
+			this.watcher.NotifyFilter = (NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.LastWrite | NotifyFilters.LastAccess);
+			this.watcher.Created += this.OnChanged;
+			this.watcher.Deleted += this.OnDeleted;
+			this.watcher.EnableRaisingEvents = true;
+			this.label1.Text = "Total Pipes: " + this.listBox1.Items.Count;
+		}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+			this.label1.Text = "Total Pipes: ";
+			this.button2.Enabled = false;
+			this.button3.Enabled = true;
+			this.watcher.Path = "\\\\.\\pipe\\";
+			this.watcher.NotifyFilter = (NotifyFilters.FileName | NotifyFilters.DirectoryName | NotifyFilters.LastWrite | NotifyFilters.LastAccess);
+			this.watcher.Created += this.OnChanged;
+			this.watcher.Deleted += this.OnDeleted;
+			this.watcher.EnableRaisingEvents = true;
+		}
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+			this.label1.Text = "Total Pipes: ";
+			this.button3.Enabled = false;
+			this.button2.Enabled = true;
+			this.watcher.EnableRaisingEvents = false;
+		}
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+			if (this.textBox1.Text == "")
+			{
+				this.textBox1.Text = "Search";
+			}
+		}
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+			if (this.textBox1.Text == "Search")
+			{
+				this.textBox1.Text = "";
+			}
+		}
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+			string text = this.textBox1.Text;
+			int num = this.listBox1.FindString(text, -1);
+			if (num != -1)
+			{
+				this.listBox1.SetSelected(num, true);
+			}
+		}
+
+        private void copyToClipboardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			Clipboard.SetText(this.listBox1.SelectedItem.ToString());
+		}
+
+        private void clearListboxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+			this.listBox1.Items.Clear();
+		}
+    }
+}
